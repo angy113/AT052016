9a8dd2343bb2f5dcd193ee7c1d28f4b18b662bf6
angy113 <rockstaradjm@gmail.com>
2016-05-08 19:57:44 -0400

    Practice 4

---
+++ Angela/Practice 4/Practice2.rb
@@ -0,0 +1,18 @@
+class Customer
+   def initialize(id, name, addr)
+      @cust_id=id
+      @cust_name=name
+      @cust_addr=addr
+   end
+   def display_details()
+      puts "Customer id #{@cust_id}"
+      puts "Customer name #{@cust_name}"
+      puts "Customer address #{@cust_addr}"
+    end
+end
+
+cust1=Customer.new("1", "Angy", "Av. Villazón CBBA")
+cust2=Customer.new("2", "Pancracio", "Achacachi")
+
+cust1.display_details()
+cust2.display_details()
