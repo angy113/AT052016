c2fee062f29bf9aa21c675c3cd2f2657a1977908
angy113 <rockstaradjm@gmail.com>
2016-05-12 16:41:51 -0400

    Coorections

--- Angela/Practice 6 Guide Line, Blocks/Passenger.rb
+++ Angela/Practice 6 Guide Line, Blocks/Passenger.rb
@@ -9,7 +9,20 @@ end
 end
 
 passenger=Passenger.new
+puts "Insert the name:"
+name=gets.chomp
+puts "Insert the price:"
+price=gets.chomp.to_f
+puts "Do you want to insert the destination? y/n"
+response=gets.chomp.to_s
 
-puts "Price in dollars: #{passenger.passenger_init "Angy",54}"
+if response=="y" then
+	puts "Insert the destination:" 
+	destination=gets.chomp.to_s 
+	priceInDollars=passenger.passenger_init name,price,destination
+else
+	priceInDollars=passenger.passenger_init name,price
+end
+puts "Price in dollars: #{priceInDollars.round(2)}"
 puts "Name: #{passenger.name}"
 puts "Destination: #{passenger.destination}"
