f0ae5e61f1a82495e7b35efd9e5fe14df22dd71b
angy113 <rockstaradjm@gmail.com>
2016-05-12 15:57:26 -0400

    Array practice

--- Angela/Practice 7 Mixin and Libraries/arrays.rb
+++ Angela/Practice 7 Mixin and Libraries/arrays.rb
@@ -1,5 +1,22 @@
+
+def array
 my_array = [1,2,5,7,11]
-#this prints the array element by element putting after an enter key
-puts my_array 
-#this prints the array in horizontal 
-puts my_array.inspect
+end
+def firsElement array
+puts array.first
+end
+def lastElement array
+puts array.last
+end
+def arrayElementsCommon array1,array2
+puts (array1&array2).inspect
+end
+
+puts "The array is:"
+puts array.inspect
+puts "The first element is:"
+firsElement array
+puts "The last element is:"
+lastElement array
+puts "The elements in common with [2,3,4,5] are :"
+arrayElementsCommon array,[2,3,4,5]
