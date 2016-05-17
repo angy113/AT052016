0ab349d5ccf867acfc48c69af2b3ead59c68347c
angy113 <rockstaradjm@gmail.com>
2016-05-17 18:02:50 -0400

    Practica 10

--- Angela/Practice 10 iterators regular expressions/regular.rb
+++ Angela/Practice 10 iterators regular expressions/regular.rb
@@ -4,7 +4,7 @@
 		while response==nil do
 			puts "Insert the username:"
 			username=gets.chomp.to_s
-			response=username=~/^[a-z]+[0-9]+[_]+$/
+			response=username=~/(?=.*[a-z])(?=.*[0-9])(?=.*[_])/
 		end 
 			
 		
@@ -14,7 +14,7 @@
 		while response==nil do
 			puts "Insert the password:"
 			username=gets.chomp.to_s
-			response=username=~/[a-z0-9A-Z]{8,16}/
+			response=username=~/(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z]).{8,16}/
 		end 
 			
 		
@@ -30,6 +30,7 @@
 			username=gets.chomp.to_s
 
 		response=username=~/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/
+		end
 	end
 userName
 password
